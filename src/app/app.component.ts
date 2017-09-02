5128cd01cd2efce27dfa77ff13330a692bb701b2
Saul Miquilena <saul.miquilena@gmail.com>
2017-09-02 16:50:56 -0400

    Sorry. Fixed app.components.ts - had problem calling firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,35 @@ import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
-  const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-    if (!user) {
-      this.rootPage = 'login';
-      unsubscribe();
-    } else {
-      this.rootPage = HomePage;
-      unsubscribe();
-    }
-  });
+  rootPage: any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-
+      
     firebase.initializeApp({
       apiKey: "",
       authDomain: "",
       databaseURL: "",
       storageBucket: "",
       messagingSenderId: ""
+    });
+
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.rootPage = 'login';
+        unsubscribe();
+      } else {
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    });
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
   }
 }
